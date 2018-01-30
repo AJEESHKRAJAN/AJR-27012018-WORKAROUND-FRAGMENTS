8b1d95067eaaa65d14dcb101f121924f98a8d72e
Ajeesh Kumar Rajan <ajeeshkumar.rajan@gmail.com>
2018-01-30 20:30:34 +0530

    Fragments - List Type - Customized - Completed

--- app/src/main/java/com/workaround/ajeesh/ajr_27012018_workaround_fragments/Fragments/FragmentListType.java
+++ app/src/main/java/com/workaround/ajeesh/ajr_27012018_workaround_fragments/Fragments/FragmentListType.java
@@ -3,6 +3,9 @@ package com.workaround.ajeesh.ajr_27012018_workaround_fragments.Fragments;
 import android.app.ListFragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import com.workaround.ajeesh.ajr_27012018_workaround_fragments.Helpers.LogHelper;
@@ -19,12 +22,19 @@ public class FragmentListType extends ListFragment {
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        LogHelper.LogThreadId(logName, "Fragment List : onActivityCreated. ");
+        //String[] courseTitlesList = getResources().getStringArray(R.array.courseTitlesForList);
+        String[] courseTitlesList = getResources().getStringArray(R.array.emptyList);
 
-        String[] courseTitlesList = getResources().getStringArray(R.array.courseTitlesForList);
-
-        ArrayAdapter<String> courseTitleAdapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_activated_1, courseTitlesList);
+        ArrayAdapter<String> courseTitleAdapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_activated_1, courseTitlesList);
 
         setListAdapter(courseTitleAdapter);
 
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        LogHelper.LogThreadId(logName, "Fragment List : onCreateView. ");
+        return inflater.inflate(R.layout.fragment_fragment_customized_list_layout, container, false);
     }
 }
